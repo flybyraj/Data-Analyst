e84ec3d76f0c4a3cdd07a36924c92ead26ec628a
flybyraj <669raj@gmail.com>
2026-04-05 21:08:09 +0530

    add

---
+++ Python/1-Python Basics/Simple_Calculator.py
@@ -0,0 +1,22 @@
+## Simple Claculator
+
+num1=float(input("Enter first Number: "))
+num2=float(input("Enter second Number: "))
+
+#Performing Arithmetic Operations
+addition= num1 + num2
+substraction = num1 - num2
+multiplication = num1 * num2
+division = num1 /num2
+floor_division = num1 // num2
+modulus = num1 % num2
+exponential = num1 ** num2
+
+#Displaying Results
+print("addition: ", addition)
+print("substraction: ", substraction)
+print("multiplication: ", multiplication)
+print("division: ", division)
+print("floor_division: ", floor_division)
+print("modulus: ", modulus)
+print("exponential: ", exponential)
